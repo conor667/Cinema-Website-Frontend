ef48fd7575464ac9456a6c42ba4b07338a2d8a3c
Conor White <conorwhite667@gmail.com>
2022-09-09 22:48:54 +0100

    added onclick functions for slider arrows

--- src/components/Pages/Images/ImageSlider.js
+++ src/components/Pages/Images/ImageSlider.js
@@ -3,17 +3,25 @@ import { SliderData } from './SliderData';
 import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa'
 
 const ImageSlider = ({slides}) => {
-const [current, setCurrent] = useState(0)
-const length = slides.length
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
 
     if(!Array.isArray(slides) || slides.length <= 0){
         return null;
     }
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1) 
+    }
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
     return(
         <section className='slider'>
-            <FaArrowAltCircleLeft className="left-arrow" />
-            <FaArrowAltCircleRight className="right-arrow"/>
+            <FaArrowAltCircleLeft className="left-arrow" onClick={prevSlide}/>
+            <FaArrowAltCircleRight className="right-arrow" onClick={nextSlide}/>
         {SliderData.map((slide, index) =>{
             return(
                 <img src={slide.image} alt="Movie banner" className='image'/>
