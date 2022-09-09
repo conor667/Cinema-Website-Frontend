c0524bc64d49cd8b494113d8478a4d1484ea2e1c
Conor White <conorwhite667@gmail.com>
2022-09-09 13:41:55 +0100

    added home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Home from './components/Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Home/>
       <header className="App-header">
       </header>
     </div>
