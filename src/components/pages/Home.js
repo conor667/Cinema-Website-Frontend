07792910b3c27df9a43bdd247b715efaca36cb7e
Conor White <conorwhite667@gmail.com>
2022-09-13 11:33:40 +0100

    updating develop with this branch

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,22 +1,9 @@
 import React from "react";
-import "./Home.css"
-import ImageSlider from "./Images/ImageSlider";
-import { SliderData } from './Images/SliderData';
 
-function Home() {
+function Home(){
     return(
-        <>
-        <div>
-        <h1> FEATURED </h1>
-        <ImageSlider slides={SliderData} />
-        <h1>WHAT'S ON</h1>
-        <p>Placeholder</p>
-        <p>Placeholder</p>
-        <p>Placeholder</p>
-        <p>Placeholder</p>
-        </div>
-        </>
+        <h1>Test</h1>
     )
 };
 
-export default Home;
+export default Home
