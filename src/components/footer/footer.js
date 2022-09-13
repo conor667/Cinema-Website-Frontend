8205a682375a38de03d8ac3a882f776f18aa6fd4
Conor White <conorwhite667@gmail.com>
2022-09-13 10:48:43 +0100

    added footer to work need to add styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,35 @@
 import React from "react";
+import "./footer.css"
 
-const footer = () => {
-    render(
+const Footer = () => {
+    return(
         <div>
-            <footer>
-                Footer
-            </footer>
+            <div className="footer-container">
+                <div class="footer">
+                    <div className="footer-heading footer-1">
+                        <h2>Information</h2>
+                        <a href="/About">About Us</a>
+                        <a href="/">Directions</a>
+                        <a href="/">Privacy Policy</a>
+                        <a href="/">Modern Slavery Statement</a>
+                    </div>
+                    <div className="footer-heading footer-2">
+                        <h2>Contact</h2>
+                        <a href="/">Contact us</a>
+                        <a href="/">Phone</a>
+                        <a href="/">Support</a>
+                    <div className="footer-heading footer-3">
+                        <h2>Social Media</h2>
+                        <a href="/">Instagram</a>
+                        <a href="/">Youtube</a>
+                        <a href="/">Twitter</a>
+                        <a href="/">Facebook</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default Footer;
