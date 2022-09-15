3a301b559ced853e7269af5d676e0e4bee5b7334
Conor White <conorwhite667@gmail.com>
2022-09-15 16:27:05 +0100

    made slight changes to lighthouse.js

--- src/components/Pages/Movies/Lighthouse.js
+++ src/components/Pages/Movies/Lighthouse.js
@@ -7,22 +7,27 @@ function Lighthouse(){
         <h1>The Lighthouse</h1>
         <img className="lighthousepic" src="https://media.gq-magazine.co.uk/photos/5e170a023720d40008ec3c55/master/pass/20200109-Ratio-03.jpg"/>
         <h2>Description</h2>
-        <p>Two lighthouse keepers try to maintain their sanity while living on a remote and mysterious New England island in the 1890s.</p>
+        <p>In a remote island, Ephraim Winslow arrives as a lighthouse keeper and assists his elderly supervisor,Thomas Wake. As days pass, Winslow is haunted by strange and mysterious visions.
+        <br/>When it is time to return home, a furious storm hits the island marooning Wake and Winslow. <br/>The two lighthouse keepers try to maintain their sanity while surviving on the remote New England island in the 1890s.</p>
         <h2>Directors</h2>
         <ul>
             <li>Robert Eggers</li>
             <li>Max Eggers</li>
         </ul>
+        <br/>
         <h2>Stars</h2>
         <ul>
             <li>Robert Pattinson</li>
             <li>Willem Dafoe</li>
         </ul>
+        <br/>
         <h2>Runtime</h2>
         <p className="runtime"> 1h 50m </p>
+        <br/>
         <h2>Trailer</h2>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/Hyag7lR8CPA" title="YouTube video player" frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br/>
         <button className="booknow"> Booknow</button>
         </div>
     )
