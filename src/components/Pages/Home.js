c6059f0c8a132e59a09fb7c21459e7ab48e8b303
Conor White <conorwhite667@gmail.com>
2022-09-15 10:28:59 +0100

    trying to fix git issue

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Home.css"
+import ImageSlider from "./Images/ImageSlider";
+import { SliderData } from './Images/SliderData';
+import HomePageImages from "./Images/HomePageImages";
 
-function Home(){
+
+function Home() {
     return(
-        <h1>Test</h1>
+    <div>
+        <h1> FEATURED </h1>
+        <ImageSlider slides={SliderData} />
+        <h1>WHAT'S ON</h1>
+        <HomePageImages />
+    </div>
     )
 };
 
-export default Home
+export default Home;
