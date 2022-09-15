81493c1472b5a2dededda3d999927dd40b6dcd31
Conor White <conorwhite667@gmail.com>
2022-09-15 16:27:23 +0100

    made slight changes to lighthouse.js

--- src/components/Pages/Movies/Lighthouse.js
+++ src/components/Pages/Movies/Lighthouse.js
@@ -9,6 +9,7 @@ function Lighthouse(){
         <h2>Description</h2>
         <p>In a remote island, Ephraim Winslow arrives as a lighthouse keeper and assists his elderly supervisor,Thomas Wake. As days pass, Winslow is haunted by strange and mysterious visions.
         <br/>When it is time to return home, a furious storm hits the island marooning Wake and Winslow. <br/>The two lighthouse keepers try to maintain their sanity while surviving on the remote New England island in the 1890s.</p>
+        <br/>
         <h2>Directors</h2>
         <ul>
             <li>Robert Eggers</li>
